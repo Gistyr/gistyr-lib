78ebbf21c452a92cb5e4ca8e7699a2696fa054a1
Lozlof <contact@gistyr.dev>
2026-02-25 09:54:18 -0500

    func

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,14 @@
 
 use std::fmt;
 
+pub const SUCCESS_CODE_U64: u64 = 0;
 pub const SUCCESS_CODE_U32: u32 = 0;
+pub const SUCCESS_CODE_U16: u16 = 0;
 pub const SUCCESS_CODE_U8: u8 = 0;
+
+pub const ERROR_CODE_U64: u64 = 1;
 pub const ERROR_CODE_U32: u32 = 1;
+pub const ERROR_CODE_U16: u16 = 1;
 pub const ERROR_CODE_U8: u8 = 1;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -30,13 +35,13 @@ impl fmt::Display for Gistyr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Success(op) => write!(f, "Success({})", op),
-            Self::Error(lib, from, msg) => write!(f, "Error({}, {}, {})", lib, from, msg),
+            Self::Error(lib, func, msg) => write!(f, "Error({}, {}, {})", lib, func, msg),
         }
     }
 }
 impl Gistyr {
-    pub fn error<L: Into<String>, F: Into<String>, M: Into<String>>(lib: L, from: F, msg: M) -> Self {
-        return Self::Error(lib.into(), from.into(), msg.into());
+    pub fn error<L: Into<String>, F: Into<String>, M: Into<String>>(lib: L, func: F, msg: M) -> Self {
+        return Self::Error(lib.into(), func.into(), msg.into());
     }
 }
 
